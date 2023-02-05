903e49a88107b9abc6fef52e609ccd691ec3cffa
Johanz Pineda <johanzpineda@gmail.com>
2023-02-05 22:51:28 +0800

    Added barangay team section

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -27,6 +27,69 @@ const AboutPage = () => {
             </div>
         </div>
     </section>
+
+    <section id="section-team" className="p-5 bg-primary">
+        <div className="container-team">
+            <h2 className="text-center text-white"> Our Barangay Team</h2>
+            <p className="lead text-center text-white mb-5">
+                Our Barangay serve and assist residents who have concerns and complaints in their residences and locations.
+            </p>
+            <div className="row g-4">
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <img 
+                            src="https://randomuser.me/api/portraits/men/15.jpg" 
+                            className="rounded-circle mb-3" alt="">
+                            </img>
+                            <h3 className='card-title mb-3'>Joshua Pineda</h3>
+                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sed, facere expedita enim porro in.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <img 
+                            src="https://randomuser.me/api/portraits/men/24.jpg" 
+                            className="rounded-circle mb-3" alt="">
+                            </img>
+                            <h3 className='card-title mb-3'>Leo Murphy</h3>
+                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sed, facere expedita enim porro in.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <img 
+                            src="https://randomuser.me/api/portraits/women/26.jpg" 
+                            className="rounded-circle mb-3" alt="">
+                            </img>
+                            <h3 className='card-title mb-3'>Juria Medina</h3>
+                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sed, facere expedita enim porro in.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <img 
+                            src="https://randomuser.me/api/portraits/women/50.jpg" 
+                            className="rounded-circle mb-3" alt="">
+                            </img>
+                            <h3 className='card-title mb-3'>Jiheon Morales</h3>
+                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sed, facere expedita enim porro in.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <Footer />
     </>
   );
