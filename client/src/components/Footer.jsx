1200f18376c96ade94c877e299a5556a5ea1cada
Johanz Pineda <johanzpineda@gmail.com>
2023-02-07 17:28:44 +0800

    updated link in copyright

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -28,9 +28,6 @@ export const Footer = () => {
                         <p>
                             <a href="/FAQs" className="text-reset">FAQs</a>
                         </p>
-                        <p>
-                            <a href="#!" className="text-reset">Support</a>
-                        </p>
                     </div>
                     {/* <!-- grid column -->
 
@@ -41,7 +38,7 @@ export const Footer = () => {
                         <p><i className="fas fa-map-marker me-3 text-secondary"></i>Cavite Philippines</p>
                         <p>
                             <i className="fas fa-envelope me-3 text-seconday"></i>
-                            example@gmail.com
+                            Barangay_Molino_3@gmail.com
                         </p>
                         <p><i className="fas fa-phone me-3 text-seconday"></i>(046) 484-0999</p>
                         <p><i className="fas fa-print me-3 text-seconday"></i>+987654321</p>
@@ -57,16 +54,8 @@ export const Footer = () => {
         <div className="text-center p-4">
             2023 Copyright: 
             <br />
-            <a className="text-reset fw-bold" href="https://facebook.com">iSumBongMo! By Johanz Robert Pineda</a>
+            <a className="text-reset fw-bold" href="https://github.com/shihansa3">iSumBongMo! By Johanz Robert Pineda</a>
         </div>
-        {/* <!-- <div className="footer">
-            <div className="row">
-                <div className="footer_inquire">form here</div>
-            </div>
-            <div className="row">
-                <div className="footer_socials">icons here</div>
-            </div>
-         </div> --> */}
     </footer>
 
     </>
