2b4893b484cbee339a524c10355153d8d9b9465e
Johanz Pineda <johanzpineda@gmail.com>
2023-02-07 10:20:53 +0800

    update: FAQs container

--- client/src/pages/FAQs.jsx
+++ client/src/pages/FAQs.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {NavbarBootstrap} from "../components/Navbarbs";
 
 const FAQs = () => {
   return (
-    <div>FAQs</div>
+    <>
+    <NavbarBootstrap />
+    <section className='faq-section'>
+      <div className='container-faq'>
+        <div className='row'>
+          <div className='col-xl-12 text-center my-5'>
+            <h1>Frequently Asked Questions</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
